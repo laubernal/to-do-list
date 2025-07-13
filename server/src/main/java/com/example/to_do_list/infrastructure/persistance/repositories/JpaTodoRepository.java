320d56340425b0a4d470234967678ca16c6e937d
laubernal <lbernalrodrguez@gmail.com>
2025-07-13 17:49:08 +0200

    fix: remove import not needed and remove repository annotation

--- server/src/main/java/com/example/to_do_list/infrastructure/persistance/repositories/JpaTodoRepository.java
+++ server/src/main/java/com/example/to_do_list/infrastructure/persistance/repositories/JpaTodoRepository.java
@@ -1,15 +1,12 @@
 package com.example.to_do_list.infrastructure.persistance.repositories;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.to_do_list.infrastructure.models.TodoEntity;
 
-@Repository
 public interface JpaTodoRepository extends JpaRepository<TodoEntity, UUID> {
     Optional<TodoEntity> findOneByTitle(String title);
 }
